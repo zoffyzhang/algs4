da961a2f330b90c78bdf833f325df87e817bd329
ZoffyZhang <zoffyzhang@foxmail.com>
2015-05-23 00:27:46 +0800

    finally fix the bugs

--- src/Chapter2/Section1/No18.java
+++ src/Chapter2/Section1/No18.java
@@ -1,9 +1,9 @@
 package Chapter2.Section1;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.geom.Line2D;
 import javax.swing.JFrame;
@@ -13,9 +13,8 @@ import edu.princeton.cs.introcs.StdRandom;
 /**
  * @Author Zoffy Zhang
  * @GitHub https://github.com/ZoffyZhang
- * @Date May 20, 2015
+ * @Date May 23, 2015
  */
-
 public class No18
 {
 	public static void main(String[] args)
@@ -25,15 +24,26 @@ public class No18
 		for (int i = 0; i < N; i++)
 			randomData[i] = StdRandom.uniform(-40.0, 60.0);
 
-		JFrame frame = new JFrame("Sort Animation");
-		frame.getContentPane().add(new SortTrackPanel(randomData, 1));
-		frame.pack();
+		JFrame frame = new JFrame("Sort Track");
+		frame.setLayout(new GridLayout(10, 1));
+		frame.setSize(600, 800);
+
+		SortTrackPanel[] panel = new SortTrackPanel[10];
+
+		for (int i = 0; i < 10; i++)
+			panel[i] = new SortTrackPanel(randomData, 0, i);
+
+		for (int i = 0; i < 10; i++)
+			frame.add(panel[i]);
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-
 	}
 }
 
+/**
+ * draw localeData , sort randomData
+ */
 class SortTrackPanel extends JPanel
 {
 	private static final long serialVersionUID = 7912515918599760257L;
@@ -46,15 +56,11 @@ class SortTrackPanel extends JPanel
 	private double yScale;
 	private int rectangleWidth = 3;
 	private int gap;				// 柱间间隔
+	private Double[] localeData;
 
-	private Double[] randomData;
-
-	public static final long FPS = 24;
-	public static final long SPF = 1000 / FPS;	// Second Per Frames
-
-	public SortTrackPanel(Double[] randomData, int gap)
+	public SortTrackPanel(Double[] randomData, int gap, int num)
 	{
-		this.randomData = randomData;
+
 		this.gap = gap;
 		int dataLen = randomData.length;
 		if (dataLen * (rectangleWidth + gap) > maxWidth)
@@ -63,8 +69,9 @@ class SortTrackPanel extends JPanel
 			rectangleWidth = (maxWidth - gap * dataLen) / dataLen;
 		}
 		else
-			width = (rectangleWidth+gap) * dataLen;
-		setPreferredSize(new Dimension(width, height));
+			width = (rectangleWidth + gap) * dataLen;
+		setSize(width, height);
+		setLocation(0, num * 80);
 
 		// 计算 y scale
 		double max = Double.MIN_VALUE;
@@ -78,38 +85,21 @@ class SortTrackPanel extends JPanel
 		}
 		max = Math.abs(max);
 		min = Math.abs(min);
-		double tmp = max > min ? max : min;
-		yScale = height / 2 / tmp;
+		max = max > min ? max : min;
+		yScale = 80 / 2 / max;
 
-		//
-		startPaintThread();
-		startSortThread();
+		Selection.sort(randomData, num);
+		drawAllRect(randomData);
 	}
 
-	private void startSortThread()
+	private void drawAllRect(Double[] randomData)
 	{
-		Selection.sort(randomData);
-	}
-
-	private void startPaintThread()
-	{
-		new Thread()
+		localeData = new Double[randomData.length];
+		for (int i = 0; i < randomData.length; i++)
 		{
-			public void run()
-			{
-				while (true)
-				{
-					try
-					{
-						repaint();
-						Thread.sleep(SPF);
-					} catch (InterruptedException e)
-					{
-						e.printStackTrace();
-					}
-				}
-			}
-		}.start();
+			localeData[i] = randomData[i];
+		}
+		repaint();
 	}
 
 	@Override
@@ -118,102 +108,47 @@ class SortTrackPanel extends JPanel
 		super.paintComponent(g);
 		setCoordinate(g);
 		drawAllRect(g);
-		drawChangineRect(g);
-	}
-
-	private void drawAllRect(Graphics g)
-	{
-		g.setColor(Color.GRAY);
-		for (int i = 0; i < randomData.length; i++)
-		{
-			// 坐标上下反转后，fill3DRect()的坐标参数也从左上角变成了左下角
-			if (randomData[i] >= 0)
-				g.fill3DRect(i * (rectangleWidth + gap), 0, rectangleWidth,
-						(int) (randomData[i] * yScale), true);
-			else
-			{
-				int leftButtomY = (int) (randomData[i] * yScale);
-				g.fill3DRect(i * (rectangleWidth + gap), leftButtomY,
-						rectangleWidth, -leftButtomY, true);
-
-			}
-		}
-	}
-
-	private void drawChangineRect(Graphics g)
-	{
-		int i = Selection.changing;
-		g.setColor(Color.RED);
-
-		if (randomData[i] >= 0)
-			g.fill3DRect(i * (rectangleWidth + gap), 0, rectangleWidth,
-					(int) (randomData[i] * yScale), true);
-		else
-		{
-			int leftButtomY = (int) (randomData[i] * yScale);
-			g.fill3DRect(i * (rectangleWidth + gap), leftButtomY,
-					rectangleWidth, -leftButtomY, true);
-
-		}
-
-		i=Selection.position;
-		g.setColor(Color.BLUE);
-
-		if (randomData[i] >= 0)
-			g.fill3DRect(i * (rectangleWidth + gap), 0, rectangleWidth,
-					(int) (randomData[i] * yScale), true);
-		else
-		{
-			int leftButtomY = (int) (randomData[i] * yScale);
-			g.fill3DRect(i * (rectangleWidth + gap), leftButtomY,
-					rectangleWidth, -leftButtomY, true);
-
-		}
 	}
 
 	private void setCoordinate(Graphics g)
 	{
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.translate(0, height / 2);	// 设置原点
-		g2d.scale(1.0, -1.0);			// 反转y轴
+		g2d.translate(0, 40);	 // 设置原点
 		g2d.draw(new Line2D.Double(0.0, 0.0, width, 0.0)); // 画x轴
+	}
+
+	private void drawAllRect(Graphics g)
+	{
+		g.setColor(Color.GRAY);
+		for (int i = 0; i < localeData.length; i++)
+		{
+			if (localeData[i] >= 0)
+				g.fill3DRect(i * (rectangleWidth + gap),
+						(int) (-localeData[i] * yScale), rectangleWidth,
+						(int) (localeData[i] * yScale), true);
+			else
+				g.fill3DRect(i * (rectangleWidth + gap), 0, rectangleWidth,
+						(int) (-localeData[i] * yScale), true);
+		}
 	}
 
 }
 
 class Selection
 {
-	protected static int changing;
-	protected static int position;
-
 	@SuppressWarnings("rawtypes")
-	public static void sort(Comparable[] a)
+	public static void sort(Comparable[] a, int sortPos)
 	{
-		new Thread()
+		for (int i = sortPos * 20; i < sortPos * 20 + 20; i++)
 		{
-			@Override
-			public void run()
-			{
-				for (int i = 0; i < a.length; i++)
-				{
-					int min = i;
-					for (int j = i + 1; j < a.length; j++)
-						if (less(a[j], a[min]))
-							min = j;
-					position=i;
-					changing = min;
-					exch(a, i, min); // 不管怎样都会执行
-					try
-					{
-						sleep(SortTrackPanel.SPF);
-					} catch (InterruptedException e)
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
-		}.start();
+			int min = i;
+			for (int j = i + 1; j < a.length; j++)
+				if (less(a[j], a[min]))
+					min = j;
+			exch(a, i, min);
+
+		}
+
 	}
 
 	@SuppressWarnings("rawtypes")
